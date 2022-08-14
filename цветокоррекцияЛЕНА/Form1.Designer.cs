511d1f6553c9e511bb7b3b86294ef4a7dfbe9e4d
HP_Note <HP_Note@COMPUTER>
2022-08-14 17:16:55 +0300

    Добавьте файлы проекта.

---
+++ цветокоррекцияЛЕНА/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace цветокоррекцияЛЕНА
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "дай картинку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Location = new System.Drawing.Point(23, 60);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(263, 187);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Location = new System.Drawing.Point(392, 60);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(263, 187);
+            this.panel2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(292, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = ">>>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 284);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "эту прогу написала ленина шиза";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ цветокоррекцияЛЕНА/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace цветокоррекцияЛЕНА
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath;
+            Bitmap bm;
+            Bitmap bm_new;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                filePath = openFileDialog1.FileName;
+                bm = new Bitmap((Bitmap)Image.FromFile(filePath), new Size(panel1.Width, panel1.Height));
+                bm_new = new Bitmap((Bitmap)Image.FromFile(filePath), new Size(panel2.Width, panel2.Height));
+
+
+                for (int i = 1; i < bm_new.Width; i++)
+                {
+                    for (int j = 1; j < bm_new.Height; j++)
+                    {
+                        if (bm.GetPixel(i, j).R >= 100 && bm.GetPixel(i, j).G <= 100 && bm.GetPixel(i, j).B < 90 && Math.Abs(bm.GetPixel(i, j).G - bm.GetPixel(i, j).B) < 85)
+                        {
+                            int q = 0; 
+                        }
+                        else
+                        {
+                            Color c = bm.GetPixel(i, j);
+                            int mxP = Math.Max(c.R, Math.Max(c.G, c.B));
+                            bm_new.SetPixel(i, j, Color.FromArgb(mxP, mxP, mxP));
+                        }
+                    }
+                }
+
+                panel1.BackgroundImage = bm;
+
+                panel2.BackgroundImage = bm_new;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
